8b348b1f9a865004bcbdcfa69a4135669203a079
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-10 15:58:51 +0200

    Aggiunstamenti card

--- template-parts/modules/module-documenti.php
+++ template-parts/modules/module-documenti.php
@@ -27,7 +27,7 @@
           ?>
           <div class="flex-hold-child card insite">
             <a href="<?php echo $doc_link; ?>" class="absl" target="_blank" title="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>" aria-label="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>"></a>
-            <div class="card_inner regular-card">
+            <div class="card_inner compact-card">
               <div class="texts-holder compact">
                 <h4 class="h4-variant"><?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?></h4>
                 <?php if ( get_sub_field( 'module_gestione_documenti_repeater_doc_description' ) ) : ?>
@@ -69,7 +69,7 @@
         ?>
         <div class="flex-hold-child card insite">
           <a href="<?php echo $doc_link; ?>" class="absl" target="_blank" title="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>" aria-label="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>"></a>
-          <div class="card_inner regular-card">
+          <div class="card_inner compact-card">
             <div class="texts-holder compact">
               <h4 class="h4-variant"><span class="title-icon icon-attachement"></span><?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?></h4>
               <?php if ( get_sub_field( 'module_gestione_documenti_repeater_doc_description' ) ) : ?>
