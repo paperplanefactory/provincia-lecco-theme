d985a1670404d43c5b1c2ffb8311f02c064cd04e
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-12 09:47:28 +0200

    Fix modulo documento

--- template-parts/modules/module-documenti.php
+++ template-parts/modules/module-documenti.php
@@ -6,7 +6,7 @@
       <div class="module-separator-flex">
         <div class="content-styled">
           <?php if ( get_sub_field( 'module_index_title_in_module' ) == 1 ) : ?>
-              <h4><span class="title-icon icon-attachement"></span><a name="indice-<?php echo $module_count; ?>" class="anchor-head"></a><?php the_sub_field( 'module_index_title' ); ?></h4>
+              <h4><a name="indice-<?php echo $module_count; ?>" class="anchor-head"></a><?php the_sub_field( 'module_index_title' ); ?></h4>
           <?php else : ?>
             <a name="indice-<?php echo $module_count; ?>" class="anchor-head"></a>
           <?php endif; ?>
@@ -29,7 +29,7 @@
             <a href="<?php echo $doc_link; ?>" class="absl" target="_blank" title="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>" aria-label="Leggi il documento <?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?>"></a>
             <div class="card_inner compact-card">
               <div class="texts-holder compact">
-                <h4 class="h4-variant"><?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?></h4>
+                <h4 class="h4-variant"><span class="title-icon icon-attachement"></span><?php the_sub_field( 'module_gestione_documenti_repeater_doc_title' ); ?></h4>
                 <?php if ( get_sub_field( 'module_gestione_documenti_repeater_doc_description' ) ) : ?>
                   <p>
                     <?php the_sub_field( 'module_gestione_documenti_repeater_doc_description' ); ?>
