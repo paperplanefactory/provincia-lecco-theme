b8cdbad7f1d3ecefd399d8b107286096c614689c
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-16 17:57:24 +0200

    Messaggio personalizzato per 404

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@ get_header();
   <div class="wrapper-padded">
     <div class="wrapper-padded-intro">
       <div class="single-content-opening-padder">
-        <h1><a href="<?php echo home_url(); ?>">404 - Pagina non trovata: tornare alla homepage?</a></h1>
+        <h1><a href="<?php echo home_url(); ?>"><?php the_field( '404_message', $acf_options_parameter ); ?></a></h1>
       </div>
     </div>
   </div>
