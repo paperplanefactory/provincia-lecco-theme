e931ff9246e0f81221aaeea78097cba6d7b76fc0
Giovanni Invernizzi <giovanni@paperplanefactory.com>
2020-06-19 12:46:00 +0200

    Fix messaggio avviso principale senza immagine

--- template-parts/grid/avviso.php
+++ template-parts/grid/avviso.php
@@ -47,34 +47,37 @@ $thumb_url_desktop = wp_get_attachment_image_src($thumb_id, 'column', true);
           </div>
         </div>
       <?php else : ?>
-        <h3 class="txt-12"><?php the_title(); ?></h3>
-        <?php if ( get_field( 'messaggio_avviso' ) ) : ?>
-          <p class="paragraph-variant">
-            <?php the_field( 'messaggio_avviso' ); ?>
-          </p>
+        <div class="avviso-padder">
+          <h3 class="txt-12"><?php the_title(); ?></h3>
+          <?php if ( get_field( 'messaggio_avviso' ) ) : ?>
+            <p class="paragraph-variant">
+              <?php the_field( 'messaggio_avviso' ); ?>
+            </p>
+          <?php endif; ?>
+          <?php
+          if ( have_rows( 'avviso_gestione_cta_repeater' ) ) : ?>
+          <div class="tags-holder">
+            <?php while ( have_rows( 'avviso_gestione_cta_repeater' ) ) : the_row();
+            $avviso_gestione_cta_repeater_destinazione = get_sub_field( 'avviso_gestione_cta_repeater_destinazione' );
+            if ( $avviso_gestione_cta_repeater_destinazione === 'internal-content' ) {
+              $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_page' );
+              $target = '_self';
+            }
+            elseif ( $avviso_gestione_cta_repeater_destinazione === 'internal-media' ) {
+              $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_file' );
+              $target = '_blank';
+            }
+            elseif ( $avviso_gestione_cta_repeater_destinazione === 'external-content' ) {
+              $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_url' );
+              $target = '_blank';
+            }
+            ?>
+            <a href="<?php echo $avviso_gestione_cta_url; ?>" target="<?php echo $target; ?>" class="square-button green filled" title="<?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?>" aria-label="<?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?>"><?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?></a>
+            <?php endwhile; ?>
+          </div>
         <?php endif; ?>
-        <?php
-        if ( have_rows( 'avviso_gestione_cta_repeater' ) ) : ?>
-        <div class="tags-holder">
-          <?php while ( have_rows( 'avviso_gestione_cta_repeater' ) ) : the_row();
-          $avviso_gestione_cta_repeater_destinazione = get_sub_field( 'avviso_gestione_cta_repeater_destinazione' );
-          if ( $avviso_gestione_cta_repeater_destinazione === 'internal-content' ) {
-            $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_page' );
-            $target = '_self';
-          }
-          elseif ( $avviso_gestione_cta_repeater_destinazione === 'internal-media' ) {
-            $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_file' );
-            $target = '_blank';
-          }
-          elseif ( $avviso_gestione_cta_repeater_destinazione === 'external-content' ) {
-            $avviso_gestione_cta_url = get_sub_field( 'avviso_gestione_cta_repeater_url' );
-            $target = '_blank';
-          }
-          ?>
-          <a href="<?php echo $avviso_gestione_cta_url; ?>" target="<?php echo $target; ?>" class="square-button green filled" title="<?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?>" aria-label="<?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?>"><?php the_sub_field( 'avviso_gestione_cta_repeater_testo' ); ?></a>
-          <?php endwhile; ?>
         </div>
-      <?php endif; ?>
+
       <?php endif; ?>
       <button onclick="closeAvvisoMain(this)" class="chiudi-avviso avviso-local-storage-close-js button-appearance-normalizer" aria-label="Nascondi avviso per il resto della navigazione">
         <span class="icon-close"></span>
