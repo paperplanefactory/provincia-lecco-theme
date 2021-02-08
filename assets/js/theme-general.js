0d44e22091baca295c8348a990e19305536402a3
paperplanefactory <giovanni@paperplanefactory.com>
2021-02-08 17:05:54 +0100

    Fix al menu di navigazione

--- assets/js/theme-general.js
+++ assets/js/theme-general.js
@@ -379,3 +379,43 @@ if ($('.sito-tematico-colors')[0]) {
     $(this).addClass(randomItem);
   });
 }
+
+
+
+
+
+
+
+
+
+$('.header-menu').mousedown(function(event) {
+  $(this)
+    .data('down', true)
+    .data('x', event.clientX)
+    .data('scrollLeft', this.scrollLeft)
+    .addClass("dragging");
+
+  return false;
+}).mouseup(function(event) {
+  $(this)
+    .data('down', false)
+    .removeClass("dragging");
+}).mousemove(function(event) {
+  if ($(this).data('down') == true) {
+    this.scrollLeft = $(this).data('scrollLeft') + $(this).data('x') - event.clientX;
+  }
+}).mousewheel(function(event, delta) {
+  this.scrollLeft -= (delta * 30);
+}).css({
+  'overflow': 'hidden',
+  'cursor': '-moz-grab'
+});
+$(window).mouseout(function(event) {
+  if ($('.team-form-data').data('down')) {
+    try {
+      if (event.originalTarget.nodeName == 'BODY' || event.originalTarget.nodeName == 'HTML') {
+        $('.team-form-data').data('down', false);
+      }
+    } catch (e) {}
+  }
+});
