6aa75be5a9be963c0b9bf229d0b1a381957ff2c4
paperplanefactory <giovanni@paperplanefactory.com>
2024-09-24 09:25:57 +0200

    Modifica widget PP backend

--- includes/theme-messages.php
+++ includes/theme-messages.php
@@ -2,45 +2,45 @@
 // Ricordarsi di abilitare il plugin paperplane-custom-welcome-email per la gestione delle e-mail di iscrizione al sito che non possono essere modificate da qui.
 
 //no messaggi di errore in login
-function no_errors_please(){
-  return '<strong>Login errato!</strong><br />Puoi provare a reimpostare la password con il link che trovi qui sotto: in caso non ricevessi l&rsquo;email contatta <a href="mailto:info@paperplanefactory.com" target="_blank">PaperPlane</a>.';
+function no_errors_please() {
+	return '<strong>Login errato!</strong><br />Puoi provare a reimpostare la password con il link che trovi qui sotto: in caso non ricevessi l&rsquo;email contatta <a href="mailto:info@paperplanefactory.com" target="_blank">PaperPlane</a>.';
 }
 add_filter( 'login_errors', 'no_errors_please' );
 
 // remove version
 function wpbeginner_remove_version() {
-return '';
+	return '';
 }
-add_filter('the_generator', 'wpbeginner_remove_version');
+add_filter( 'the_generator', 'wpbeginner_remove_version' );
 
 // admin footer
-function remove_footer_admin () {
-echo 'Powered by <a href="https://www.wordpress.org" target="_blank">WordPress</a> | Handcrafted by <a href="https://www.paperplanefactory.com" target="_blank">Paperplane</a></p>';
+function remove_footer_admin() {
+	echo 'Powered by <a href="https://www.wordpress.org" target="_blank">WordPress</a> | Handcrafted by <a href="https://www.paperplanefactory.com" target="_blank">Paperplane</a></p>';
 }
-add_filter('admin_footer_text', 'remove_footer_admin');
+add_filter( 'admin_footer_text', 'remove_footer_admin' );
 
 // admin help box
-add_action('wp_dashboard_setup', 'my_custom_dashboard_widgets');
+add_action( 'wp_dashboard_setup', 'my_custom_dashboard_widgets' );
 function my_custom_dashboard_widgets() {
-global $wp_meta_boxes;
-wp_add_dashboard_widget('custom_help_widget', 'Supporto per il tuo sito', 'custom_dashboard_help');
+	global $wp_meta_boxes;
+	wp_add_dashboard_widget( 'custom_help_widget', 'Supporto per il tuo sito', 'custom_dashboard_help' );
 }
 function custom_dashboard_help() {
-echo '<a href="http://www.paperplanefactory.com" target="_blank"><img src="' . get_template_directory_uri() . '/assets/images/admin-images/logo-paper.jpg" width="200" /></a><h2>Benvenuto nelll&rsquo;area di amministrazione del tuo sito!</h2><p>Il sito <strong>' . get_bloginfo('name') . '</strong> è basato su <a href="https://wordpress.org/" target="_blank">WordPress</a> e utilizza un tema appositamente creato da <a href="http://www.paperplanefactory.com" target="_blank">Paper Plane Factory.</a><br />
+	echo '<a href="http://www.paperplanefactory.com" target="_blank"><img src="' . get_template_directory_uri() . '/assets/images/admin-images/logo-paper.svg" width="200" /></a><h2>Benvenuto nelll&rsquo;area di amministrazione del sito.</h2><p>Il sito <strong>' . get_bloginfo( 'name' ) . '</strong> è basato su <a href="https://wordpress.org/" target="_blank">WordPress</a> e utilizza un tema appositamente creato da <a href="http://www.paperplanefactory.com" target="_blank">PaperPlane Factory.</a><br />
 Puoi consulare il Manuale di uso del sito istituzionale della Provincia di Lecco a <a href="https://manuale.provincia.lecco.it/wp-login.php" target="_blank"><strong>questo link</strong></a>.<br />
-<strong>Hai bisogno di aiuto? <a href="mailto:info@paperplanefactory.com">Contattaci</a>!!</strong></p>';
+<strong>Hai bisogno di aiuto? <a href="mailto:info@paperplanefactory.com">Contattaci</a></strong> via mail tramite il link oppure scrivi a info@paperplanefactory.com</p>';
 }
 
 // remove junk from head
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wp_generator');
-remove_action('wp_head', 'feed_links', 2);
-remove_action('wp_head', 'index_rel_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links_extra', 3);
-remove_action('wp_head', 'start_post_rel_link', 10, 0);
-remove_action('wp_head', 'parent_post_rel_link', 10, 0);
-remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 );
 
 // custom login
 add_filter( 'login_headerurl', 'namespace_login_headerurl' );
@@ -53,6 +53,6 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 // login custom logo
 function namespace_login_style() {
-    echo '<style>.login h1 a { background-image: url( ' . get_template_directory_uri() . '/assets/images/admin-images/logo-login.png ) !important; }</style>';
+	echo '<style>.login h1 a { background-image: url( ' . get_template_directory_uri() . '/assets/images/admin-images/logo-login.png ) !important; }</style>';
 }
 add_action( 'login_head', 'namespace_login_style' );
